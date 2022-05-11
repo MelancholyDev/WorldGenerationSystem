88852875f794d77ca4e8e0087f023eea5966c234
MelancholyDev <morriganisdream@gmail.com>
2022-05-11 18:35:56 +0300

    fix path

---
+++ Plugins/SimplexNoise/Intermediate/Build/Win64/UE4Editor/Inc/SimplexNoise/SimplexNoiseBPLibrary.generated.h
@@ -0,0 +1,116 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SIMPLEXNOISE_SimplexNoiseBPLibrary_generated_h
+#error "SimplexNoiseBPLibrary.generated.h already included, missing '#pragma once' in SimplexNoiseBPLibrary.h"
+#endif
+#define SIMPLEXNOISE_SimplexNoiseBPLibrary_generated_h
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_50_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FCell_Statics; \
+	SIMPLEXNOISE_API static class UScriptStruct* StaticStruct();
+
+
+template<> SIMPLEXNOISE_API UScriptStruct* StaticStruct<struct FCell>();
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_43_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FTriangle_Statics; \
+	SIMPLEXNOISE_API static class UScriptStruct* StaticStruct();
+
+
+template<> SIMPLEXNOISE_API UScriptStruct* StaticStruct<struct FTriangle>();
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_31_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FPoint_Statics; \
+	SIMPLEXNOISE_API static class UScriptStruct* StaticStruct();
+
+
+template<> SIMPLEXNOISE_API UScriptStruct* StaticStruct<struct FPoint>();
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_SPARSE_DATA
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_RPC_WRAPPERS
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_RPC_WRAPPERS_NO_PURE_DECLS
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUSimplexNoiseBPLibrary(); \
+	friend struct Z_Construct_UClass_USimplexNoiseBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(USimplexNoiseBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/SimplexNoise"), NO_API) \
+	DECLARE_SERIALIZER(USimplexNoiseBPLibrary)
+
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_INCLASS \
+private: \
+	static void StaticRegisterNativesUSimplexNoiseBPLibrary(); \
+	friend struct Z_Construct_UClass_USimplexNoiseBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(USimplexNoiseBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/SimplexNoise"), NO_API) \
+	DECLARE_SERIALIZER(USimplexNoiseBPLibrary)
+
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USimplexNoiseBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USimplexNoiseBPLibrary) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USimplexNoiseBPLibrary); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USimplexNoiseBPLibrary); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USimplexNoiseBPLibrary(USimplexNoiseBPLibrary&&); \
+	NO_API USimplexNoiseBPLibrary(const USimplexNoiseBPLibrary&); \
+public:
+
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USimplexNoiseBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USimplexNoiseBPLibrary(USimplexNoiseBPLibrary&&); \
+	NO_API USimplexNoiseBPLibrary(const USimplexNoiseBPLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USimplexNoiseBPLibrary); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USimplexNoiseBPLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USimplexNoiseBPLibrary)
+
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_PRIVATE_PROPERTY_OFFSET
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_57_PROLOG
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_PRIVATE_PROPERTY_OFFSET \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_SPARSE_DATA \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_RPC_WRAPPERS \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_INCLASS \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_PRIVATE_PROPERTY_OFFSET \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_SPARSE_DATA \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_RPC_WRAPPERS_NO_PURE_DECLS \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_INCLASS_NO_PURE_DECLS \
+	SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h_60_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class SimplexNoiseBPLibrary."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SIMPLEXNOISE_API UClass* StaticClass<class USimplexNoiseBPLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SurvivalGame_Plugins_SimplexNoise_Source_SimplexNoise_Public_SimplexNoiseBPLibrary_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
